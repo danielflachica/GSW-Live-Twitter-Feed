7a6892273e356e79f60ea29b60537a9870a2e98d
Daniel Lachica <daniel.lachica82@gmail.com>
2021-04-13 14:34:59 +0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [
+  	'./*.html',
+  	'./src/js/*.js',
+  	'./src/img/*.*',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
